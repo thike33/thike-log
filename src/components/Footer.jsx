b84989b7eb8a8e3931e992fbf265d06c9d77cd42
thike33 <teke3business@gmail.com>
2023-06-12 21:52:45 +0900

    💄 ヘッダーとフッターのレイアウト調整

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer>
-      <small>©︎2023 てぃけログ</small>
+    <footer className="bg-blue-800 text-center">
+      <small className="inline-block p-3 text-base font-bold text-white">
+        ©︎2023 てぃけログ
+      </small>
     </footer>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
   return (
-    <header>
-      <h1>てぃけログ</h1>
+    <header className="bg-blue-500 py-5">
+      <div className="m-auto flex w-full max-w-7xl items-center justify-between px-5">
+        <h1 className="text-2xl font-bold text-white">
+          <Link href="/">てぃけログ</Link>
+        </h1>
+        <nav>
+          <ul className="flex items-center gap-5">
+            <li>
+              <Link href="/" className="text-base font-bold text-white">
+                トップ
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="text-base font-bold text-white">
+                カテゴリー
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
